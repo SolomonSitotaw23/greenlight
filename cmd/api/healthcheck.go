9cde694d34ce77e6032dae6cb4fb260f516edf64
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-06 08:29:34 -0300

    REFACTOR: move writeJson into a healper function for further handlers

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	js := `{"status":"available","environment":%q,"version":%q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	// change the default content type to application/json
-	w.Header().Set("Content-Type", "application/json")
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
 
-	w.Write([]byte(js))
+	// marshal data in to JSON
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -8,7 +9,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// Retrive the "id" URL parameter from the current request context
+// Retrieve the "id" URL parameter from the current request context
 func (app *application) readIdParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
@@ -17,4 +18,27 @@ func (app *application) readIdParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id parameter")
 	}
 	return id, nil
+}
+
+// helper for sending responses this takes the destination http.ResponseWritter and the http status code
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	//Encode the data to json returning error if there was one.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Append new line to make it easier for terminal applications
+	js = append(js, '\n')
+	// loop through the header map and add each header to the http.ResponseWriter header map.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Add content type application/json then write the status code and json response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
