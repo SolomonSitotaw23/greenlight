5204a5a3b024bc5e3e03ed366968cec11210f2c9
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-06 10:44:40 -0300

    ADD: Movie struct and return it in the movie handler

---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID        int64     `json:"id"`                      //unique integer ID
+	CreatedAt time.Time `json:"-"`                       // Timestamp for when the movie is added to our database // this isn't relevant for end users
+	Title     string    `json:"title"`                   //Movie title
+	Year      int32     `json:"year,omitzero"`           // Movie release year
+	Runtime   int32     `json:"runtime,omitzero,string"` //movie runtime in minutes
+	Genres    []string  `json:"genres,omitempty"`        //Slice of genres for the movie
+	Version   int32     `json:"version"`                 // starts at 1 and will be incremented each time the movie information is updated
+}
