fdaeaa65823799d4ef5824c9dd64bcf75e93cbf3
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-08 06:21:34 -0300

    ADD custom encoder for the runtime json property

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
@@ -13,4 +17,27 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue) //wrap in a quote before returning it
 
 	return []byte(quotedJSONValue), nil
+}
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) (err error) {
+	// remove the surrounding quote
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	// split the string to isolate the part containing the number
+	parts := strings.Split(unquotedJSONValue, " ")
+	// sanity check the parts of the string
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+	// parse the number into int32
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+	// convert the int32 into a runtime type and assign this to the receiver
+	*r = Runtime(i)
+
+	return nil
 }
