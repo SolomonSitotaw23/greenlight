606190841cea6f45afacdddae35cb135550a320c
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-12 05:02:03 -0300

    ADD: create movie handler created

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/solomonsitotaw23/greenlight/internal/validator"
 )
 
@@ -33,4 +35,37 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 
+}
+
+// define a movie model
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// insert a movie
+func (m MovieModel) Insert(movie *Movie) error {
+	query := `
+	INSERT INTO movies (title,year,runtime,genres)
+	VALUES ($1,$2,$3,$4)
+	RETURNING id,created_at,version
+	`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
+
+// fetch a movie
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+// update a movie
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+// delete a movie
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
