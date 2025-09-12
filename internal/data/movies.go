1adbc582f5321464d2265959049b62119f2845d5
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-12 07:10:01 -0300

    ADD update movies endpoit

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -92,7 +92,22 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 // update a movie
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	query := `
+	UPDATE movies
+	SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+	WHERE id = $5
+	RETURNING version
+`
+
+	args := []any{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.Version)
 }
 
 // delete a movie
