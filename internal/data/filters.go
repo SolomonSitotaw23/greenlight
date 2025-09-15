00aa8fd33086c068c29a0a102299a74622b2300b
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-15 14:47:28 -0300

    ADD: pagenation

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -22,6 +22,8 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(validator.PermittedValue(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
 }
 
+// Check that the client-provided Sort field matches one of the entries in our safelist
+// and if it does
 func (f Filters) sortColumn() string {
 	for _, safeValue := range f.SortSafelist {
 		if f.Sort == safeValue {
@@ -39,4 +41,12 @@ func (f Filters) sortDirection() string {
 		return "DESC"
 	}
 	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
