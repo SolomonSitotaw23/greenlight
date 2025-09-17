90735f10d08bbb34828ef8abcae220019f8cd7ef
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-17 04:35:21 -0300

    ADD rate limiter mutex to prevent race condition on the client Ip on memory mao

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
@@ -31,10 +32,33 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 // Global and Ip rate limiter
 func (app *application) rateLimit(next http.Handler) http.Handler {
 
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+
 	var (
 		mu      sync.Mutex
-		clients = make(map[string]*rate.Limiter)
+		clients = make(map[string]*client)
 	)
+
+	// A background goroutine which removes old entries from the clients map once
+	// every minute.
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			// Lock the mutex to prevent any rate limiter checks from happening while
+			// the cleanup is taking place.
+			mu.Lock()
+
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ip := realip.FromRequest(r)
@@ -43,12 +67,15 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		mu.Lock()
 
 		if _, found := clients[ip]; !found {
-			clients[ip] = rate.NewLimiter(2, 4)
+			// Create and add a new client struct to the map if it doesn't already exist.
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
 		}
+		// update the lastseen time for the client
+		clients[ip].lastSeen = time.Now()
 		// Call the Allow() method on the rate limiter for the current IP address. If
 		// the request isn't allowed, unlock the mutex and send a 429 Too Many Requests
 		// response
-		if !clients[ip].Allow() {
+		if !clients[ip].limiter.Allow() {
 			mu.Unlock()
 			app.rateLimitExceededResponse(w, r)
 			return
