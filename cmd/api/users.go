403dc9c9cee0446ef30ba446151245382dbd5fa7
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-09-21 06:30:04 -0300

    REFACTOR: move send email to go routine to run in backgroung

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -51,14 +51,16 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		}
 		return
 	}
+	// run send email in the background to decouple from the normal request
+	go func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", user)
+		if err != nil {
+			app.logger.Error(err.Error())
 
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+		}
+	}()
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
